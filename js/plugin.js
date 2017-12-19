b5392be3c757708dac6a4b3017b1f08024725222
Guillermo García Galindo <gekubo@me.com>
2017-12-19 17:13:45 +0100

    Version 0.3
    Added new experiment with jQuery

--- js/plugin.js
+++ js/plugin.js
@@ -1,4 +1,47 @@
 /*TRAFFICLIKE*/
+$( document ).ready( function() {
+
+  $( ".button" ).click( function() {
+/*    console.log($( "#red" ).hasClass( "on" ));
+    if ($( "#red" ).hasClass( "on" ) == false &&
+        $( "#orange" ).hasClass( "on" ) == false &&
+        $( "#green" ).hasClass( "on" ) == false) {
+
+      $( "#red" ).addClass( "on" );
+     console.log($( "#red" ).hasClass( "on" ));
+    }*/
+
+    if ($( "#red" ).hasClass( "on" ) == true) {
+      $( "#red" ).removeClass( "on" );
+
+      $( "#orange" ).addClass( "on" );
+    }
+
+    else if ($( "#orange" ).hasClass( "on" ) == true) {
+      $( "#orange" ).removeClass( "on" );
+
+      $( "#green" ).addClass( "on" );
+    }
+
+    else {
+      $( "#green" ).removeClass( "on" );
+
+      $( "#red" ).addClass( "on" );
+    }
+
+  });
+
+});
+
+
+/*$(document).ready(function(){
+  $("input[name=trafficSwitch]").change(function () {
+    $(".red").addClass("on");
+    $(".orange").addClass("off");
+    $(".green").addClass("off");
+  });
+});
+*/
 
 /*var trafficLight = document.getElementById();
 
@@ -22,6 +65,7 @@ switch ( document.getElementsByName("fname");) {
 
 }*/
 
+/*
 // definir una variable universal para elegir el elemento en el que se va a prducir el cambio
 var element = document.getElementById('id');
 
@@ -34,6 +78,7 @@ function changeColor(){
     }
     element.style.opacity = "0.9" = document.trafficLight.trafficSwitch[i].value
 }
+*/
 
 // function trafficLight() {
 
@@ -64,7 +109,7 @@ function changeColor(){
 //     trafficLight.("red").style.opacity = "0.5";
 //     trafficLight.("orange").style.opacity = "0.5";
 //     trafficLight.("green").style.opacity = "1";
-}
+
 
 /*var interruptor = false;
 if(interruptor != false) {
@@ -74,3 +119,117 @@ if(interruptor != false) {
     interruptor = false;
 }
 }*/
+
+/* TABBIMG */
+
+$( document ).ready( function() {
+
+/*CAMBIO DE IMÁGENES AL HACER CLIC*/
+function change () {
+
+}
+
+  $( "#button1" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).show();
+    $( "#image2" ).hide();
+    $( "#image3" ).hide();
+
+  });
+
+  $( "#button2" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).hide();
+    $( "#image2" ).show();
+    $( "#image3" ).hide();
+
+  });
+
+  $( "#button3" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#image1" ).hide();
+    $( "#image2" ).hide();
+    $( "#image3" ).show();
+
+  });
+
+/*AÑADIENDO Y ELIMINANDO CLASES CSS*/
+
+/*  $( "#button1" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).removeClass( "hide" );
+    $( "#image2" ).addClass( "hide" );
+    $( "#image3" ).addClass( "hide" );
+
+  });
+
+  $( "#button2" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).addClass( "hide" );
+    $( "#image2" ).removeClass( "hide" );
+    $( "#image3" ).addClass( "hide" );
+
+  });
+
+  $( "#button3" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#image1" ).addClass( "hide" );
+    $( "#image2" ).addClass( "hide" );
+    $( "#image3" ).removeClass( "hide" );
+
+  });*/
+
+/*USANDO LAS FUNCIONES SHOW / HIDE DE JQUERY*/
+
+/*  $( "#button1" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).show();
+    $( "#image2" ).hide();
+    $( "#image3" ).hide();
+
+  });
+
+  $( "#button2" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button3" ).removeClass( "btnhover" );
+    $( "#image1" ).hide();
+    $( "#image2" ).show();
+    $( "#image3" ).hide();
+
+  });
+
+  $( "#button3" ).click( function() {
+
+    $( this ).addClass( "btnhover" );
+    $( "#button1" ).removeClass( "btnhover" );
+    $( "#button2" ).removeClass( "btnhover" );
+    $( "#image1" ).hide();
+    $( "#image2" ).hide();
+    $( "#image3" ).show();
+
+  });
+*/
+});
